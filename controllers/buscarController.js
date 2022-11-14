265d79653c13dd41dd6db2a533ff84889a2a3867
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-11-13 20:43:59 -0500

    added buscador of products and categories

--- controllers/buscarController.js
+++ controllers/buscarController.js
@@ -8,6 +8,12 @@ const { Usuario, Categoria, Producto } = require("../models");
 /* Variables */
 const coleccionesPermitidas = ["usuarios", "categorias", "productos", "roles"];
 
+/**
+ * Busca un usuario en la base de datos por su nombre o correo electrónico
+ * @param [termino] - El término a buscar.
+ * @param [res] - respuesta
+ * @returns Una matriz de usuarios.
+ */
 const buscadorUsuarios = async (termino = "", res = response) => {
   const esMongoID = ObjectID.isValid(termino); // True o false
 
@@ -33,6 +39,72 @@ const buscadorUsuarios = async (termino = "", res = response) => {
   });
 };
 
+/**
+ * Busca una categoría en la base de datos.
+ * @param [termino] - El término a buscar.
+ * @param [res] - respuesta
+ * @returns Una matriz de objetos.
+ */
+const buscarCategoria = async (termino = "", res = response) => {
+  const esMongoID = ObjectID.isValid(termino); // True o false
+
+  /* Comprobando si el `terminal` es un ID de MongoDB válido. Si es así, devolverá al usuario con esa ID. */
+  if (esMongoID) {
+    const categoria = await Categoria.findById(termino);
+    return res.json({
+      results: categoria ? [categoria] : [],
+    });
+  }
+
+  /* Está creando una expresión regular que se usará para buscar el `término` en la base de datos. */
+  const regex = new RegExp(termino, "i");
+
+  /* Buscando el `término` en la base de datos. */
+  const categoria = await Categoria.find({
+    $or: [{ nombre: regex }],
+  });
+
+  return res.status(200).json({
+    status: true,
+    results: categoria ? [categoria] : [],
+  });
+};
+
+/**
+ * Si el `término` es una ID de MongoDB válida, devuelva al usuario con esa ID. De lo contrario,
+ * devuelva los usuarios cuyo nombre coincida con `terminal`.
+ * </código>
+ * @param [termino] - El término de búsqueda.
+ * @param [res] - respuesta
+ * @returns Una matriz de objetos.
+ */
+const buscarProductos = async (termino = "", res = response) => {
+  const esMongoID = ObjectID.isValid(termino); // True o false
+
+  /* Comprobando si el `terminal` es un ID de MongoDB válido. Si es así, devolverá al usuario con esa ID. */
+  if (esMongoID) {
+    const producto = await Producto.findById(termino)
+      .populate("categoria", "nombre")
+      .populate("usuario", "nombre");
+
+    return res.status(200).json({
+      status: true,
+      results: producto ? [producto] : [],
+    });
+  }
+
+  /* Crear una expresión regular que se usará para buscar el `término` en la base de datos. */
+  const regex = new RegExp(termino, "i");
+
+  /* Crear una expresión regular que se usará para buscar el `término` en la base de datos. */
+  const productos = await Producto.find({ nombre: regex });
+
+  return res.status(200).json({
+    status: true,
+    results: productos ? [productos] : [],
+  });
+};
+
 const buscar = (req, res = response) => {
   const { coleccion, termino } = req.params;
 
@@ -43,13 +115,17 @@ const buscar = (req, res = response) => {
     });
   }
 
+  /* Una declaración de cambio. Se utiliza para realizar diferentes acciones basadas en diferentes
+  condiciones. */
   switch (coleccion) {
     case "usuarios":
       buscadorUsuarios(termino, res);
       break;
     case "categorias":
+      buscarCategoria(termino, res);
       break;
     case "productos":
+      buscarProductos(termino, res);
       break;
     case "roles":
       break;
